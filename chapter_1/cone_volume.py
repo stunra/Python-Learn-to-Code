691452046f187124e70997cc3991bfc59d7ec358
Morgan <moscocarpenter@gmail.com>
2021-09-02 15:28:23 -0400

    Added chapter 1 practice problems and ids

--- chapter_1/cone_volume.py
+++ chapter_1/cone_volume.py
@@ -1,11 +1,9 @@
-#https://dmoj.ca/problem/dmopc14c5p1
-
 PI = 3.141592653589793
 
-radius = int(input())
-height = int(input())
+r = int(input())
+h = int(input())
 
-cone_volume = (PI * radius ** 2 * height) / 3
+answer = (PI * r ** 2 * h) / 3
 
-print(cone_volume)
+print(answer)
 
--- chapter_1/word_count.py
+++ chapter_1/word_count.py
@@ -1,5 +1,3 @@
-#https://dmoj.ca/problem/dmopc15c7p2
-
 line = input()
 total_words = line.count(' ') + 1
 print(total_words)
