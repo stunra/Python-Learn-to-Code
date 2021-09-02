a08d397e30e0d2d84d74e9dd376881d272c68344
Morgan <moscocarpenter@gmail.com>
2021-09-02 14:50:35 -0400

    Updated problem ids and added comments at top of files

---
+++ chapter_1/cone_volume.py
@@ -0,0 +1,11 @@
+#https://dmoj.ca/problem/dmopc14c5p1
+
+PI = 3.141592653589793
+
+radius = int(input())
+height = int(input())
+
+cone_volume = (PI * radius ** 2 * height) / 3
+
+print(cone_volume)
+
